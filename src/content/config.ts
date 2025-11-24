3ff077f22d87fd3365f9d313896a22e6bb50711d
AlejandroTC <aletamayo-02@hotmail.com>
2025-11-24 16:49:49 -0600

    [CHANGE] experiences layout and saved data

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,5 @@
 import { defineCollection, z } from "astro:content";
 
-const experiences = defineCollection({
-  schema: z.object({
-    position: z.string(),
-    company: z.string(),
-    site: z.string().optional(),
-    location: z.string(),
-    country: z.string(),
-    from: z.number(),
-    to: z.number().optional(),
-  }),
-});
-
 const education = defineCollection({
   schema: z.object({
     school: z.object({
@@ -27,4 +15,4 @@ const education = defineCollection({
   }),
 });
 
-export const collections = { experiences, education };
+export const collections = { education };
